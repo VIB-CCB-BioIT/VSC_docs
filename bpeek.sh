9daa5945bc45d161bff7f7a0298244a21a4d4aa5
Luuk Harbers <40829819+ljwharbers@users.noreply.github.com>
2024-07-25 14:54:03 +0200

    Create bpeek.sh

---
+++ bpeek.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# take as input an argument - slurm job id - and save it into a variable
+jobid=$1
+# run scontrol show job $jobid and save the output into a variable
+#find the string that starts with StdOut= and save it into a variable without the StdOut= part
+stdout=$(scontrol show job $jobid | grep StdOut= | sed 's/StdOut=//')
+#show last 10 rows of the file if no argument 2 is given
+nrows=${2:-10}
+tail -f -n $nrows $stdout
