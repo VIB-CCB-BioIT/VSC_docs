e8d386396ebd29711e102262b78c46adaf429955
Luuk Harbers <40829819+ljwharbers@users.noreply.github.com>
2024-08-07 13:27:15 +0200

    add --cluster wice

--- files/bpeek.sh
+++ files/bpeek.sh
@@ -3,7 +3,7 @@
 jobid=$1
 # run scontrol show job $jobid and save the output into a variable
 #find the string that starts with StdOut= and save it into a variable without the StdOut= part
-stdout=$(scontrol show job $jobid | grep StdOut= | sed 's/StdOut=//')
+stdout=$(scontrol show job $jobid --cluster wice | grep StdOut= | sed 's/StdOut=//')
 #show last 10 rows of the file if no argument 2 is given
 nrows=${2:-10}
 tail -f -n $nrows $stdout
